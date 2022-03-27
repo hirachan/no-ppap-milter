9383ce4df231e5710744080dd9428e9f8091bb96
root <yo@hirano.cc>
2022-03-27 23:26:26 +0900

    remove unused code

--- no_ppap_milter/no_ppap_milter.py
+++ no_ppap_milter/no_ppap_milter.py
@@ -1,7 +1,8 @@
 import sys
-from typing import BinaryIO, Tuple, Union, Any
+from typing import Tuple, Union, Any
 import tempfile
 import socket
+from io import BytesIO
 
 import Milter
 
@@ -13,41 +14,17 @@ from .libemail import EnvelopeInfo, has_encrypted_zip
 
 @Milter.header_leading_space
 class NoPPAPMilter(Milter.Base):
-    envinfo: EnvelopeInfo
-    fp: BinaryIO
+    fp: BytesIO
 
     @Milter.noreply
     def connect(self, hostname: str, family: socket.AddressFamily, hostaddr: Union[Tuple[str, int], Tuple[str, int, int, int], str]) -> Any:
-        print(f"{hostname}, {family}, {hostaddr}")
+        print(f"Connected from: {hostname}({hostaddr})")
         sys.stdout.flush()
-
-        self.envinfo = EnvelopeInfo(hostaddr[0])
-
-        return Milter.CONTINUE
-
-    def hello(self, helo: str) -> Any:
-        self.envinfo.helo = helo
 
         return Milter.CONTINUE
 
     def envfrom(self, mail_from: str, *opts):
-        if mail_from and mail_from != "<>":
-            if mail_from[0] == "<":
-                mail_from = mail_from[1:-1]
-        self.envinfo.mail_from = mail_from
-
         self.fp = tempfile.TemporaryFile("wb+")
-
-        return Milter.CONTINUE
-
-    @Milter.noreply
-    def envrcpt(self, rcpt_to, *opts):
-        if rcpt_to:
-            if rcpt_to[0] == "<":
-                rcpt_to = rcpt_to[1:-1]
-        self.envinfo.rcpt_tos.append(rcpt_to)
-
-        print(self.envinfo)
 
         return Milter.CONTINUE
 
@@ -72,7 +49,6 @@ class NoPPAPMilter(Milter.Base):
         return Milter.CONTINUE
 
     def eom(self):
-        # qid = self.getsymval("i")
         self.fp.seek(0)
         if has_encrypted_zip(self.fp):
             self.setreply('550', '5.7.1', 'We do not accpet encrypted zip.')
